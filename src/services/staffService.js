004c0c7e225521e0d4a6392837e42004898c8bf7
Yaseen Ahmed <yaseen@bytes4sale.com>
2026-08-25 21:43:44 +0800

    updated service

--- src/services/staffService.js
+++ src/services/staffService.js
@@ -2,5 +2,18 @@ import API from "./api";
 
 export const getStaff = (businessId) => API.get(`/v1/staff/${businessId}`);
 export const storeStaff = (data) => API.post("/v1/staff", data);
-export const updateStaff = (id, data) => API.put(`/v1/staff/${id}`, data);
+export const updateStaff = (id, data) => {
+  const formData = new FormData();
+  Object.keys(data).forEach((key) => {
+    if (Array.isArray(data[key])) {
+      data[key].forEach((item) => formData.append(`${key}[]`, item));
+    } else {
+      formData.append(key, data[key]);
+    }
+  });
+  formData.append("_method", "PUT");
+  return API.post(`/v1/staff/${id}`, formData, {
+    headers: { "Content-Type": "multipart/form-data" },
+  });
+};
 export const deleteStaff = (id) => API.delete(`/v1/staff/${id}`);
